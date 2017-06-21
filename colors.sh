c6a067e4427794c19c65128f2d33897f62df1f39
Nebula-Mechanica <nebula_mechanica@fake.fake>
2017-06-21 16:00:15 +0700

    Readme update and small fix

--- colors.sh
+++ colors.sh
@@ -8,12 +8,12 @@ symbol () {
 sed -n "${1}p" < alphabet;
 }
 
-for i in {2..62};
+for i in {1..62};
 do
 var=$(color "$i");
 varc=$(symbol "$i");
 #echo $var;
-sed s/green/$var/ -i work/col$i;
+sed s/COLOR/$var/ -i work/col$i;
 sed s/\\/people\\/a/\\/people\\/$varc/ -i work/col$i;done
 cat common > fullcol
 cat work/col{1..62} >> fullcol

4415c39b9b282724a73a3ff31424adb7afc7a08e
nebularia <nebularia@test.test>
2019-11-21 22:57:20 +0700

    Reworked project structure

---
+++ generate.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+DATADIR="data"
+TEMPDIR="template"
+WORKDIR="work"
+mkdir $WORKDIR
+for i in {1..62};do
+	cp $TEMPDIR/pattern $WORKDIR/col$i
+done
+color () {
+	sed -n "${1}p" < $DATADIR/colors
+}
+symbol () {
+	sed -n "${1}p" < $DATADIR/alphabet
+}
+
+calculate () {
+	color="${1}"
+	#color=$(echo ${color^^}|sed "s/#//g")
+	hash=$(echo $color| md5sum -)
+	hash=${hash^^}
+	hash=${hash:0:6}
+	#echo $hash >> log
+	decimal=$(echo "ibase=16;$hash"|bc)
+	result=$(echo "(($decimal%100)*1.5)/1+55"|bc)
+	echo $result
+}
+
+for i in {1..62};
+do
+	#echo $i
+	current_color=$(color "$i")
+	current_symbol=$(symbol "$i")
+	current_rotation=$(calculate "$current_color")
+	sed s/_COLOR_/$current_color/ -i $WORKDIR/col$i
+	sed s/_ROTATE_/$current_rotation/ -i $WORKDIR/col$i
+	sed s/\\/people\\/a/\\/people\\/$current_symbol/ -i $WORKDIR/col$i
+	sed s/a\\/profile/$current_symbol\\/profile/ -i $WORKDIR/col$i
+done
+#echo "end"
+cat $TEMPDIR/common > lor-coloravs.css
+cat $WORKDIR/col{1..62} >> lor-coloravs.css
+cat $TEMPDIR/end >> lor-coloravs.css
+#echo "going to remove $WORKDIR/"
+rm $WORKDIR/col*
+rmdir $WORKDIR
